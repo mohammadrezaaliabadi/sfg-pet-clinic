802f7eda16a14319d2d78f02cd43cf2552dde50e
mohammadrezaaliabadi <mohammadrezaaliabadi9@gmail.com>
2021-11-19 13:15:22 +0330

    Initial commit

---
+++ src/main/groovy/com/example/sfgpetclinic/SfgPetClinicApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinic
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SfgPetClinicApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication, args)
+    }
+
+}
